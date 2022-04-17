5ad448bf2354e0602fd1642a574711ac50b1918c
naimxno <naimxno@gmail.com>
2022-04-17 14:06:47 +0600

    nav and banner complete

---
+++ src/Pages/Home/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import logo from "../../../Images/logo.png"
+
+const Header = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <div>
+          <img className='w-25 mx-2' src={logo} alt="" />
+          <Navbar.Brand href="#home">In Focus</Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Services</Nav.Link>
+            <Nav.Link href="#pricing">Services</Nav.Link>
+            <Nav.Link href="#pricing">Services</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Login</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">Sign in</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
